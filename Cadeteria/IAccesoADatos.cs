ccada93d8c26004673750ab52ae69b502fecf3e7
SantiagoAMS <74890008+SantiagoAMS@users.noreply.github.com>
2025-09-06 12:04:42 -0300

    Delete Cadeteria directory

--- Cadeteria/IAccesoADatos.cs
+++
@@ -1,8 +0,0 @@
-using Cadeteria;
-
-public interface IAccesoADatos
-{
-    public List<Pedido> CargaPedidos(string ruta);
-    public List<Cadete> CargaCadetes(string ruta);
-    public Cadeteria.Cadeteria CargaCadeteria(string ruta);
-}
