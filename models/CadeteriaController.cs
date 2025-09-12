e7320759d9a7aebeaeea8a7251949b5bc76bdc4f
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-09-12 10:50:32 -0300

    usecontroller

---
+++ models/CadeteriaController.cs
@@ -0,0 +1,13 @@
+public class CadeteriaController
+{
+    
+    /*
+    [Get] GetPedidos() => Retorna una lista de Pedidos  
+    [Get] GetCadetes() => Retorna una lista de Cadetes  
+    [Get] GetInforme() => Retorna un objeto Informe  
+    [Post] AgregarPedido(Pedido pedido)  
+    [Put] AsignarPedido(int idPedido, int idCadete) 
+    [Put] CambiarEstadoPedido(int idPedido,int NuevoEstado)  
+    [Put] CambiarCadetePedido(int idPedido,int idNuevoCadete) 
+    */
+}
